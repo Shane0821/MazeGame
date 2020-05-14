007033ced0d30249d9a33a362fdc327b02734804
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-14 22:22:55 +0800

    amended AppDelegate.cpp

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -21,7 +21,7 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
  ****************************************************************************/
-
+#include <bits/stdc++.h>
 #include "AppDelegate.h"
 #include "HelloWorldScene.h"
 
